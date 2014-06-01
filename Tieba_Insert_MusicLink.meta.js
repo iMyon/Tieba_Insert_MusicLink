ecdf824290ad77be3513f4dc0f841a2f4b7d4d3d
1005911753@qq.com <1005911753@qq.com>
2014-06-01 11:02:08 +0800

    脚本更新

---
+++ Tieba_Insert_MusicLink.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Tieba_Insert_MusicLink
+// @namespace   tieba
+// @description 贴吧自定义插入mp3链接
+// @include     http://tieba.baidu.com/*
+// @version     1.0.1
+// @author	  初代作者为校长之怒/修复（绯色）
+// @updateURL   https://nightlyfantasy.github.io/Tieba_Insert_MusicLink/Tieba_Insert_MusicLink.meta.js
+// @downloadURL https://nightlyfantasy.github.io/Tieba_Insert_MusicLink/Tieba_Insert_MusicLink.user.js
+// ==/UserScript==
